20b1b3414453b3a3936ff4fc0fbb1f616350ba9a
Omar Ahmed <75138543+OmarAhmed0001@users.noreply.github.com>
2023-02-25 00:38:55 +0200

    first

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React  from "react";
+
+
+
+const App = ()=>{
+    const bstyle={"color":"red","marginLeft":"40%","marginTop":"20%"};
+    return (
+        <div>
+            <h1 style={bstyle}> hello world from app</h1>
+        </div>
+    )
+
+}
+ 
+export default App ;
